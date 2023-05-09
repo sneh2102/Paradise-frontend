590db81f4ac0425ce049069d6152558b60d545f8
Sneh Patel <patel.sneh2102@gmail.com>
2023-05-09 11:26:50 +0530

    secont commit

--- src/pages/Shared/Header/index.jsx
+++ src/pages/Shared/Header/index.jsx
@@ -46,7 +46,7 @@ const Header = () => {
     <header>
       <nav>
         <Link to="/">
-          <img src={logo} className="logo" />
+          <h3>Paradise</h3>
         </Link>
         <ul>
           <li>
